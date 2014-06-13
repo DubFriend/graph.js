d1df6a814d9055f21bc1f73b0aa0eae2a6b611c3
Brian Detering <bdeterin@gmail.com>
2014-06-13 00:51:55 -0700

    find by id and depthFirstSearch

--- graph.js
+++ graph.js
@@ -7,7 +7,9 @@
 
 		var mapNode = function (node) {
 			var mapLink = function (link) {
-				return _.isObject(link) ? link : { id: link, weight: undefined };
+				return _.isObject(link) ? link : {
+					id: link, weight: undefined
+				};
 			};
 
 			return {
@@ -20,10 +22,6 @@
 			};
 		};
 
-		// var isIDInPreLinks = function (preLinks, id) {
-		// 	return _.indexOf(_.pluck(preLinks, 'id'), id) !== -1;
-		// };
-
 		var getPreLinkByID = function (preLinks, id) {
 			var index = _.indexOf(_.pluck(preLinks, 'id'), id);
 			return index === -1 ? null : preLinks[index];
@@ -33,11 +31,13 @@
 
 		var graph = list.shift();
 
-		var referenceList = [graph];
+
+		var referenceDictionary = {};
+		referenceDictionary[graph.id] = graph;
 
 		_.each(list || [], function (node) {
 
-			_.each(referenceList, function (reference) {
+			_.each(referenceDictionary, function (reference) {
 				var link = getPreLinkByID(reference.preLinks, node.id);
 				if(link) {
 					reference.links.push({
@@ -55,23 +55,23 @@
 				}
 			});
 
-			referenceList.push(node);
+			referenceDictionary[node.id] = node;
 		});
 
-		_.each(referenceList, function (reference) {
+		_.each(referenceDictionary, function (reference) {
 			delete reference.preLinks;
 		});
 
 		return {
 			graph: graph,
-			referenceList: referenceList
+			referenceDictionary: referenceDictionary
 		};
 	};
 
 	var GraphJS = function (list) {
 		var results = mapListToGraph(list);
 		this.graph = results.graph;
-		this.referenceList = results.referenceList;
+		this.referenceDictionary = results.referenceDictionary;
 	};
 
 	if(module && module.exports) {
@@ -81,6 +81,36 @@
 	else {
 		this.GraphJS = GraphJS;
 	}
+
+	GraphJS.prototype.depthFirstSearch = function (isMatch) {
+		var depthFirstRecurse = function (node, id) {
+			node.discovered = true;
+
+			// if(node.id === id) {
+			if(isMatch(node)) {
+				return node;
+			}
+
+			return _.reduce(node.links, function (acc, link) {
+				if(!link.ref.discovered) {
+					return acc || depthFirstRecurse(link.ref, isMatch);
+				}
+				return acc || null;
+			}, null);
+		};
+
+		var results = depthFirstRecurse(this.graph, isMatch);
+
+		_.each(this.referenceDictionary, function (reference) {
+			delete reference.discovered;
+		});
+
+		return results;
+	};
+
+	GraphJS.prototype.find = function (id) {
+		return this.referenceDictionary[id] || null;
+	};
 
 	GraphJS.prototype.hasCycles = function () {
 
--- test.js
+++ test.js
@@ -1,22 +1,90 @@
 var GraphJS = require('./graph');
 var _ = require('underscore');
 
-exports.construction = function (test) {
-	var graph = new GraphJS([
+exports.setUp = function (done) {
+	this.graph = new GraphJS([
 		{ id: 1, data: 'a', link: 2 },
 		{ id: 2, data: 'b', link: [1, { id: 3, weight: 5 }] },
 		{ id: 3, data: 'c', link: 1 }
 	]);
+	done();
+};
 
-	test.strictEqual(graph.graph.data, 'a', 'a');
-	test.strictEqual(graph.graph.links[0].ref.data, 'b', 'a.b');
-	test.strictEqual(graph.graph.links[0].ref.links[0].ref.data, 'a', 'a.b.a');
-	test.strictEqual(graph.graph.links[0].ref.links[1].ref.data, 'c', 'a.b.a.c');
-	test.strictEqual(graph.graph.links[0].ref.links[1].weight, 5, 'a.b.a.c (weight)');
-	test.strictEqual(graph.graph.links[0].ref.links[1].ref.links[0].ref.data, 'a', 'a.b.a.c.a');
-	test.deepEqual(_.pluck(graph.referenceList, 'data'), ['a', 'b', 'c'], 'referenceList has elements');
-	test.strictEqual(graph.referenceList[0], graph.graph, 'referenceList 0');
-	test.strictEqual(graph.referenceList[1], graph.graph.links[0].ref, 'referenceList 1');
-	test.strictEqual(graph.referenceList[2], graph.graph.links[0].ref.links[1].ref, 'referenceList 2');
+exports.construction = function (test) {
+
+	test.strictEqual(this.graph.graph.data, 'a', 'a');
+
+	test.strictEqual(this.graph.graph.links[0].ref.data, 'b', 'a.b');
+
+	test.strictEqual(
+		this.graph.graph.links[0].ref.links[0].ref.data,
+		'a', 'a.b.a'
+	);
+
+	test.strictEqual(
+		this.graph.graph.links[0].ref.links[1].ref.data,
+		'c', 'a.b.a.c'
+	);
+
+	test.strictEqual(
+		this.graph.graph.links[0].ref.links[1].weight, 5,
+		'a.b.a.c (weight)'
+	);
+
+	test.strictEqual(
+		this.graph.graph.links[0].ref.links[1].ref.links[0].ref.data,
+		'a', 'a.b.a.c.a'
+	);
+
+	test.deepEqual(
+		// _.pluck(this.graph.referenceList, 'data'),
+		Object.keys(this.graph.referenceDictionary),
+		['1', '2', '3'],
+		'referenceList has elements'
+	);
+
+	test.strictEqual(this.graph.referenceDictionary['1'], this.graph.graph, 'referenceList 0');
+
+	test.strictEqual(
+		this.graph.referenceDictionary['2'],
+		this.graph.graph.links[0].ref,
+		'referenceList 1'
+	);
+
+	test.strictEqual(
+		this.graph.referenceDictionary['3'],
+		this.graph.graph.links[0].ref.links[1].ref,
+		'referenceList 2'
+	);
+
+	test.done();
+};
+
+exports.depthFirstSearch = function (test) {
+	test.strictEqual(this.graph.depthFirstSearch(function (node) {
+		return node.data === 'a';
+	}).data, 'a');
+
+	test.strictEqual(this.graph.depthFirstSearch(function (node) {
+		return node.data === 'b';
+	}).data, 'b');
+
+	test.strictEqual(this.graph.depthFirstSearch(function (node) {
+		return node.data === 'c';
+	}).data, 'c');
+
+	test.strictEqual(this.graph.depthFirstSearch(function (node) {
+		return node.data === 'wrong';
+	}), null);
+
+	test.done();
+};
+
+exports.find = function (test) {
+	test.strictEqual(this.graph.find(1).data, 'a');
+	test.strictEqual(this.graph.find(2).data, 'b');
+	test.strictEqual(this.graph.find('3').data, 'c');
+	test.strictEqual(this.graph.find(3).data, 'c');
+	test.strictEqual(this.graph.find(4), null);
 	test.done();
 };
